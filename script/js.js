f956515f0bee3212eee80c9fc886d50e6f788ed6
mardnerafael <mardenrafaeldalmagro.gimenez@gmail.com>
2020-09-16 20:18:21 -0400

    fim da section sobre

---
+++ script/js.js
@@ -0,0 +1,8 @@
+const pg = document.querySelector("html") 
+let resp = confirm("confirme para continuar")
+
+if (resp == true){
+    pg.style.visibility = "visible"
+}else{
+    pg.style.visibility = "hidden"
+}
