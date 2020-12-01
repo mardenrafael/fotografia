23cfe11ba6d2beb8d90f6e21acb141637fe92e41
Marden <Mardenrafaeldalmagro.gimenez@gmail.com>
2020-11-30 20:07:48 -0400

    arquivo JS excluido

--- js/js.js
+++
@@ -1,8 +0,0 @@
-const pg = document.querySelector("html") 
-let resp = confirm("confirme para continuar")
-
-if (resp == true){
-    pg.style.visibility = "visible"
-}else{
-    pg.style.visibility = "hidden"
-}
